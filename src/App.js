906fd51b90399693e8064412b2e794858ab48175
Gary Hughes <gclm.automation@outlook.com>
2022-08-11 22:48:52 +0100

    Finished Pokedex project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Pokecard from "./Components/Pokecard"
+import Pokegame from './Components/Pokegame'
 
 
 class App extends Component {
   render() {
     return (
     <div className="App">
-      <Pokecard 
-        id={4}
-        name="Charmander"
-        type="Fire"
-        exp={62}
-      />
+      <Pokegame />
     </div>
     );
   } 
--- src/Components/Pokecard.js
+++ src/Components/Pokecard.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import '../css/Pokecard.css';
 
-const POKE_API =  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+const POKE_API =  "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
+
+let padToThree = (number) => (number <= 999 ? (`00${number}`.slice(-3)) : number)
 
 class Pokecard extends Component {
   render() {
-    let imageSrc = `${POKE_API}${this.props.id}.png`;
+    let imageSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
     return (
       <div className='Pokecard'>
-        <h1>{this.props.name}</h1>
-        <img src={imageSrc} alt={this.props.name} />
-        <div>Type : {this.props.type}</div>
-        <div>Exp : {this.props.exp}</div>
+        <h1 className="Pokecard-title">{this.props.name}</h1>
+        <img className='Pokecard-image' src={imageSrc} alt={this.props.name} />
+        <div className="Pokecard-data">Type : {this.props.type}</div>
+        <div className='Pokecard-data'>Exp : {this.props.exp}</div>
       </div>
     )
   }
